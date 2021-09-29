f64747438a2fca27634d6d9b395585283683dbb6
dazl finger <dazl.fngr@gmail.com>
2021-09-29 16:47:54 -0400

    added info header to JS and CSS

---
+++ assets/seqmatch.js
@@ -0,0 +1,7 @@
+/*********************************
+ * fn: seqmatch.css
+ * by: danConcepion
+ * dt: 2021-09-29T16:46:05
+ * pn: Sequence Match Game
+ * co: Indy Attic Software
+ ********************************/
