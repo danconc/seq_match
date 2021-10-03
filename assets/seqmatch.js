e370dfd253202ce20d27e396fa28b736b9247d08
dazl finger <dazl.fngr@gmail.com>
2021-10-03 16:34:03 -0400

    added lighted button functionality; added jquery

--- assets/seqmatch.js
+++ assets/seqmatch.js
@@ -5,3 +5,44 @@
  * pn: Sequence Match Game
  * co: Indy Attic Software
  ********************************/
+'use strict';
+/*
+var audio = new Audio('audio_file.mp3');
+audio.play();
+*/
+
+var seqm = {
+    color: '',
+    playTone : function() {},
+    btnClick: function () { //alert('click');
+        var btn = $(this),
+            tmp = null;
+
+        console.log(btn.attr('id'));
+
+        btn.addClass('clicked');
+        tmp = setTimeout(function(){
+            btn.removeClass('clicked');
+        }, 250);
+    },
+    loadHandlers: function () {
+        $('#buttonGrp').find('button').on('click', seqm.btnClick);
+    },
+    init: function () { //alert('Ready Player One...');
+        seqm.loadHandlers();
+    }
+};
+
+$(function () {
+    //alert('Ready player one...');
+    seqm.init();
+});
+
+/*
+document.addEventListener("DOMContentLoaded", function(event) 
+{
+	//document is fully loaded 
+})
+
+https://www.w3schools.com/jsref/met_win_settimeout.asp
+*/
